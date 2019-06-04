80f11aee81b40fc398d585e99c58bd8f16b788c0
KhallilB <khallil.bailey@gmail.com>
2019-06-04 09:53:02 -0700

    writing out dependencies

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const server = express();
