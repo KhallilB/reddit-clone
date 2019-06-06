4e032bd825a2cc992e1490654bea9dbd8d66b1dd
KhallilB <khallil.bailey@gmail.com>
2019-06-06 09:03:59 -0700

    validating posts

---
+++ Backend/validation/posts.js
@@ -0,0 +1,32 @@
+const Validator = require('validator');
+const isEmpty = require('is-empty');
+
+function validateReminder(post) {
+  let errors = {};
+
+  // Convert fields to an empty string to use validator functions
+  post.title = !isEmpty(post.title) ? post.title : '';
+  post.url = !isEmpty(post.url) ? post.url : '';
+  post.description = !isEmpty(post.description) ? post.description : '';
+
+  // Check Title
+  if (Validator.isEmpty(post.title)) {
+    errors.title = 'Title is Required';
+  }
+
+  // Check Url
+  if (Validator.isEmpty(post.url)) {
+    error.url = 'Url is Required';
+  } else if (!Validator.isURL(post.url)) {
+    errors.url = 'Url is not valid';
+  }
+
+  // Check Description
+  if (Validator.isEmpty(post.description)) {
+    errors.description = 'Description is Required]';
+  }
+}
+
+module.exports = {
+  validateReminder
+};
