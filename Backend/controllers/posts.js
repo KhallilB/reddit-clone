e2a16612ef4f00c3942b72c4af906de2014aeb1e
KhallilB <khallil.bailey@gmail.com>
2019-06-06 08:14:49 -0700

    create a post in controller

---
+++ Backend/controllers/posts.js
@@ -0,0 +1,31 @@
+const Post = require('../models/Post');
+
+//*** Creates a new post and saves it to database
+//---------------------------------------------------------
+const newPost = async (req, res) => {
+  try {
+    // Define new post object
+    const post = await new Post();
+
+    // Set post model properties to form values
+    post.title = req.body.title;
+    post.url = req.body.url;
+    post.description = req.body.description;
+
+    console.log(`Post waiting to be saved: ${post}`);
+
+    // Save post
+    await post.save();
+
+    console.log(`Saved post: ${post}`);
+
+    return res.json({ reminder }).status(200);
+  } catch (err) {
+    console.log('Error: ', err);
+    return res.send(err).status(500);
+  }
+};
+
+module.exports = {
+  newPost
+};
