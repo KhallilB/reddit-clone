6c53fcc0985e176381562b72e0865a6665bc17df
KhallilB <khallil.bailey@gmail.com>
2019-06-09 14:29:27 -0700

    comment model

---
+++ Backend/models/Comment.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  content: {
+    type: String,
+    required: [true, 'Content is required in comment']
+  }
+});
+
+module.exports = Comment = ('CommentSchema', CommentSchema);
