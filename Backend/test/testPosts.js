7e7db8e6f038edba5f2cea8f0e2183bb232127f1
KhallilB <khallil.bailey@gmail.com>
2019-06-09 13:31:12 -0700

    tests

--- Backend/test/testPosts.js
+++ Backend/test/testPosts.js
@@ -1,17 +1,31 @@
+const server = require('../server');
 const sinon = require('sinon');
 const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
 const expect = chai.expect;
 
 const mongoose = require('mongoose');
 require('sinon-mongoose');
 
+chai.use(chaiHttp);
+
 const Post = require('../models/Post');
+
+// Empty database before each test
+describe('Books', () => {
+  beforeEach(done => {
+    Book.remove({}, err => {
+      done();
+    });
+  });
+});
 
 // Creating a new post (POST)
 //---------------------------------------------------------
-// Should pass if post is saved
-describe('Creating a new post', function() {
-  it('should create a new post an return success', function(done) {
+describe('Creating a new post', () => {
+  // Should pass if post is created successfully
+  it('should create a new post an return success', done => {
     // Create a mock post
     const PostMock = sinon.mock(
       new Post({
@@ -25,7 +39,7 @@ describe('Creating a new post', function() {
 
     PostMock.expects('save').yields(null, result);
 
-    post.save(function(err, result) {
+    post.save((err, result) => {
       PostMock.verify();
       PostMock.restore();
       expect(result.status).to.be.equal(200);
@@ -34,4 +48,45 @@ describe('Creating a new post', function() {
   });
 });
 
-// Should pass of post is not saved
+// Getting all posts (GET)
+//---------------------------------------------------------
+describe('Get all posts', () => {
+  // Should pass if all of the posts are recieved
+  it('should return all posts and return success', done => {
+    //Create mock post
+    const PostMock = sinon.mock(Post);
+    const result = { status: 200, posts: [] };
+
+    PostMock.expects('find').yields(null, result);
+
+    Post.find((err, result) => {
+      PostMock.verify();
+      PostMock.restore();
+      expect(result.status).to.be.equal(200);
+      done();
+    });
+  });
+});
+
+// Get a specific post (GET) (Not passing)
+//---------------------------------------------------------
+describe('Get a specific post', () => {
+  // Should pass if getting a post by its id
+  it('should return a single post and return sucess', done => {
+    const PostMock = sinon.mock(Post);
+    const result = { status: 200, post: [] };
+
+    PostMock.expects('find')
+      .withArgs({ _id: 12345 })
+      .yields(null, result);
+
+    Post.findById((err, result) => {
+      PostMock.verify();
+      PostMock.restore();
+      expect(result.status).to.be.equal(200);
+      done();
+    });
+  });
+});
+
+//
