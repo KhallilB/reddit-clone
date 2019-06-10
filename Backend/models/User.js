d42a64b031c6b2fcb1c7364a854bb9f3e8e2cda2
KhallilB <khallil.bailey@gmail.com>
2019-06-10 14:47:04 -0700

    editing user model

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now()
+  },
   email: {
     type: String,
     required: [true, 'Email Is Required']
