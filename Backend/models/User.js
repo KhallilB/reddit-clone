7323e9fa5a815bc895b760fbd981da5199ce6530
KhallilB <khallil.bailey@gmail.com>
2019-06-10 14:56:24 -0700

    installed bcrypt refined user model

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -12,10 +13,21 @@ const UserSchema = new Schema({
   },
   email: {
     type: String,
-    required: [true, 'Email Is Required']
+    required: [true, 'Email Is Required'],
+    match: [
+      /^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+      'Enter a vaild email'
+    ]
   },
   password: {
     type: String,
-    required: [true, 'Password is Required']
+    required: [true, 'Password is Required'],
+    minlength: 8,
+    match: [
+      /^(?=.*\d).{8,24}$/,
+      'Password must be between 8 and 24 characters long and include at least one numeric character'
+    ]
   }
 });
+
+module.exports = User = mongoose.model('UserSchema', UserSchema);
