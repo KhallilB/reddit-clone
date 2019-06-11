ce594f627dcc59e2bdb35dd9a26446b73e33834d
KhallilB <khallil.bailey@gmail.com>
2019-06-10 18:42:19 -0700

    validating signup

---
+++ Backend/validation/signUp.js
@@ -0,0 +1,30 @@
+const Validator = require('validator');
+const isEmpty = require('is-empty');
+
+const validateSignup = user => {
+  let errors = {};
+
+  // Convert fields to an empty string to use validator functions
+  user.username = !isEmpty(user.username) ? user.username : '';
+  user.email = !isEmpty(user.email) ? user.email : '';
+  user.password = !isEmpty(user.password) ? user.password : '';
+
+  // Chek username
+  if (Validator.isEmpty(user.username)) {
+    errors.username = 'Username is required';
+  }
+
+  // Check email
+  if (Validator.isEmpty(user.email)) {
+    errors.email = 'Email is required';
+  }
+
+  // Check password
+  if (Validator.isEmpty(user.password)) {
+    errors.password = 'Password is required';
+  }
+};
+
+module.exports = {
+  validateSignup
+};
